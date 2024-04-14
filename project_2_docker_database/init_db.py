821aeddca1de32b26b975d678a20ca109571a09e
Rich Unite <uniterich@gmail.com>
2024-04-14 14:07:31 -0700

    Setup DB

--- project_2_docker_database/init_db.py
+++ project_2_docker_database/init_db.py
@@ -18,70 +18,16 @@ cursor = db.cursor()
 # Create a Compliments table (wrapping it in a try-except is good practice)
 try:
   cursor.execute("""
-    CREATE TABLE Compliments (
-      id          integer  AUTO_INCREMENT PRIMARY KEY,
-      first_name        VARCHAR(60) NULL DEFAULT NULL,
-      last_name         VARCHAR(60) NULL DEFAULT NULL,
-      chef_first_name   VARCHAR(60) NOT NULL,
-      chef_last_name    VARCHAR(60) NOT NULL,
-      city              VARCHAR(60) NOT NULL, 
-      email             VARCHAR(60) NOT NULL,
-      anonymous         BOOLEAN,
-      mailing_list      BOOLEAN,
-      created_at        DATETIME
+    CREATE TABLE CUSTOMER_LIST (
+      id                 integer  AUTO_INCREMENT PRIMARY KEY,
+      coordinates        VARCHAR(60) NULL DEFAULT NULL,
+      invite             TINYINT(1)
     );
   """)
   print("Creating Compliments table...")
 except:
   print("Compliments Table already exists. Not recreating it.")
 
-
-# Create a mailing_list table
-try:
-  cursor.execute("""
-    CREATE TABLE mailing_list (
-      id  integer AUTO_INCREMENT PRIMARY KEY,
-      first_name     VARCHAR(60) NULL DEFAULT NULL,
-      last_name      VARCHAR(60) NULL DEFAULT NULL,
-      mobile         VARCHAR(15) NULL DEFAULT NULL,
-      email          VARCHAR(60) NOT NULL,
-      created_at     DATETIME
-    );
-  """)
-  print("Creating mailing_list table...")
-except:
-  print("mailing_list Table already exists. Not recreating it.")
-
-# # Create a Clients table (wrapping it in a try-except is good practice)
-# try:
-#   cursor.execute("""
-#     CREATE TABLE Customers (
-#       id integer  AUTO_INCREMENT PRIMARY KEY,
-#       first_name  VARCHAR(30) NOT NULL,
-#       last_name   VARCHAR(30) NOT NULL,
-#       email       VARCHAR(50) NOT NULL,
-#       created_at  TIMESTAMP
-#     );
-#   """)
-# except:
-#   print("Table already exists. Not recreating it.")
-
-# # Create a Restaurants table (wrapping it in a try-except is good practice)
-# try:
-#   cursor.execute("""
-#     CREATE TABLE Customers (
-#       id integer  AUTO_INCREMENT PRIMARY KEY,
-#       first_name  VARCHAR(30) NOT NULL,
-#       last_name   VARCHAR(30) NOT NULL,
-#       email       VARCHAR(50) NOT NULL,
-#       created_at  TIMESTAMP
-#     );
-#   """)
-# except:
-#   print("Table already exists. Not recreating it.")
-
-# Selecting Records
-cursor.execute("select * from Compliments;")
 
 print('---------- DATABASE INITIALIZED ----------')
 [print(x) for x in cursor]
